fcb3dec1c6d61868ede61a2e758496ccad80132d
pharned <harned.exchange@gmail.com>
2022-08-08 20:22:03 -0500

    order changes in satellite.ts

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -2,16 +2,16 @@ export class Satellite {
 
 	name: string;
 	type: string;
-	launchDate: string;
-	orbitType: string;
 	operational: boolean;
+	orbitType: string;
+	launchDate: string;
 
 	constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
 		this.name = name;
 		this.type = type;
-		this.launchDate = launchDate;
-		this.orbitType = orbitType;
 		this.operational = operational;
+		this.orbitType = orbitType;
+		this.launchDate = launchDate;
    }
 	
 	isSpaceDebris(): boolean {
