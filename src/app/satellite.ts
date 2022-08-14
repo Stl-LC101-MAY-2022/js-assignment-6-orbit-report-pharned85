4c645798b70df26c5f62917dddaa91de1a9d57a7
pharned <harned.exchange@gmail.com>
2022-08-14 18:36:52 -0500

    steps 1-2 done and everything showing in correct space on table

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -2,22 +2,21 @@ export class Satellite {
 
 	name: string;
 	type: string;
-	operational: boolean;
-	orbitType: string;
 	launchDate: string;
+	orbitType: string;
+	operational: boolean;
 
 	constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
 		this.name = name;
 		this.type = type;
-		this.operational = operational;
-		this.orbitType = orbitType;
 		this.launchDate = launchDate;
+		this.orbitType = orbitType;
+		this.operational = operational;
    }
-	
-	isSpaceDebris(): boolean {
-		return true;
+   
+	isSpaceDebris() {
+		return this.type === 'Space Debris';
    }
 
 }
-
 // TODO 3a: fix isSpaceDebris check
